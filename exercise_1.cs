da6f41ac2fd1b12cb65a974f9011175cd6e36692
Alexis Correa <alexis.correa026@gmail.com>
2023-06-19 00:14:13 -0300

    Fix - Se captura excepcion al ingresar un dato que no sea de tipo INT

--- exercise_1.cs
+++ exercise_1.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +7,28 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
     class Program
-    { 
+    {
         static void Main(string[] args)
-        { 
+        {
             int[] numbers = new int[3];
-            
-            for(int i =0; i < numbers.Length; i++)
+            bool breakLoop = false;
+
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine("Enter a number: ");
-                numbers[i] = int.Parse(Console.ReadLine());
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter a number: ");
+                        numbers[i] = Convert.ToInt32(Console.ReadLine());
+                        breakLoop = true;
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine("Please enter a valid number.");
+                        breakLoop = false;
+                    }
+                } while (!breakLoop);
             }
 
             Array.Sort(numbers);
@@ -24,6 +36,6 @@ namespace ConsoleApp1
             Console.WriteLine("The smallest number is: chico es: " + numbers[0]);
             Console.WriteLine("The median number is:: " + numbers[1]);
             Console.WriteLine("The largest number is: " + numbers[2]);
-        } 
-    } 
-} 
+        }
+    }
+}
